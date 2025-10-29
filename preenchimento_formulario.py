0a38ed86afa62a7693b048e06a9e16605e02b97b
Filipe <filipe.candido.rodrigues@gmail.com>
2025-10-29 12:31:04 -0300

    first commit

---
+++ preenchimento_formulario.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import logging
+from datetime import datetime
+
+#Configurações do arquivo de log
+
